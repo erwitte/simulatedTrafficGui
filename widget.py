368ba3f63609888d916344d7f32a645dd3abcffc
erwitte <108674109+erwitte@users.noreply.github.com>
2024-02-27 11:57:06 +0000

    final gui

--- widget.py
+++ widget.py
@@ -1,29 +1,44 @@
-import random
-from json_parser import JsonParser
+import webbrowser
+
+from data_parser import DataParser
 from script_caller import ScriptCaller
 
 from PySide6 import QtWidgets
-from PySide6.QtWidgets import QLabel, QSpinBox, QComboBox
+from PySide6.QtWidgets import QLabel, QSpinBox, QComboBox, QCheckBox
 
 
 class MyWidget(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
+        self.checkbox_layout = None
+        self.input_duration = None
         self.script_caller = ScriptCaller()
-        self.json_parser = JsonParser("~/Schreibtisch/alles/PycharmProjects/simulatedTraffic/10.json")
+        self.data_parser = DataParser("~/Schreibtisch/alles/PycharmProjects/simulatedTraffic/10.json")
         self.layout = QtWidgets.QVBoxLayout(self)
+        self.populate()
+
+    def populate(self):
         self.create_choose_day()
         self.create_choose_speed()
         self.create_choose_display_duration()
+        self.create_open_file_checkbox()
 
-        # button leitet einträge weiter und löscht fensterinhalt
+        # button leitet einträge weiter und lädt farbbalken
         confirm_button = QtWidgets.QPushButton("Bestätigen", self)
         confirm_button.clicked.connect(self.clear_contents)
         self.layout.addWidget(confirm_button)
 
+    def populate_after_1st_time(self):
+        # deletes back btn
+        # index keeps changing so loop necessary
+        for i in reversed(range(self.layout.count())):
+            widget = self.layout.itemAt(i).widget()
+            if widget is not None:
+                widget.setParent(None)
+        self.populate()
 
     def create_choose_day(self):
-        number_days = self.json_parser.get_number_of_days()
+        number_days = self.data_parser.get_number_of_days()
         choose_days_label = QLabel("Tag wählen")
         choose_days_label.setFixedSize(200, 30)
         self.layout.addWidget(choose_days_label)
@@ -56,10 +71,10 @@ class MyWidget(QtWidgets.QWidget):
         duration_label.setFixedSize(200, 30)
         self.layout.addWidget(duration_label)
 
-        input_duration = QSpinBox()
-        input_duration.setMinimum(0)
-        input_duration.setFixedSize(100, 30)
-        self.layout.addWidget(input_duration)
+        self.input_duration = QSpinBox()
+        self.input_duration.setMinimum(0)
+        self.input_duration.setFixedSize(100, 30)
+        self.layout.addWidget(self.input_duration)
 
         combobox_duration = QComboBox()
         combobox_duration.addItem("Minuten")
@@ -67,11 +82,45 @@ class MyWidget(QtWidgets.QWidget):
         combobox_duration.setFixedSize(100, 30)
         self.layout.addWidget(combobox_duration)
 
+    def create_open_file_checkbox(self):
+        self.checkbox_layout = QtWidgets.QHBoxLayout(self)
+
+        open_file_label = QLabel("Datei automatisch öffnen")
+        open_file_label.setFixedSize(200, 30)
+        self.checkbox_layout.addWidget(open_file_label)
+
+        open_file_checkbox = QCheckBox()
+        open_file_checkbox.setChecked(False)
+        self.checkbox_layout.addWidget(open_file_checkbox)
+
+        self.layout.addLayout(self.checkbox_layout)
+
+    def get_input(self):
+        inputted_values = []
+        print(self.input_duration.value())
+
     def clear_contents(self):
+        self.get_input()
         self.script_caller.start_script()
-        # Clear all child widgets from the layout
+        webbrowser.open("marker.html")
+        # Clear all child widgets from layout
         for i in reversed(range(self.layout.count())):
             widget = self.layout.itemAt(i).widget()
             if widget is not None:
                 widget.setParent(None)
+        # clears all child widgets fron checkbox_layout
+        for i in reversed(range(self.checkbox_layout.count())):
+            widget = self.checkbox_layout.itemAt(i).widget()
+            if widget is not None:
+                widget.setParent(None)
+
+        back_btn = QtWidgets.QPushButton("zurück", self)
+        back_btn.clicked.connect(self.populate_after_1st_time)
+        self.layout.addWidget(back_btn)
+
+    def call_script(self):
+        print("isjg")
+
+
+
 
